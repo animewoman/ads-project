6dcd4465f1a8997c47c1bd9a6c83d897dc53e211
animewoman <bekameilun@gmail.com>
2020-02-04 20:51:12 +0600

    asdfsdfsdfdsfsd

--- index.js
+++ index.js
@@ -1,20 +1,41 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 5000;
 
-const ROUTES = require('./route');
+const ROUTES = require("./route");
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(ROUTES);
-mongoose.connect(
-    process.env.DB_CONNECT,
-    {useNewUrlParser: true},
-    () => console.log('connected to DB!'),
+app.use(function(req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () =>
+  console.log("connected to DB!")
 );
 
-app.listen(PORT, () => console.log('server up and running....'));
-
+app.listen(PORT, () => console.log("server up and running...."));
