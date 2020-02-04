5c7bd9dcaed24845e89b30aff6202f70751e3860
animewoman <bekameilun@gmail.com>
2020-02-04 21:14:50 +0600

    trying cors

--- index.js
+++ index.js
@@ -10,7 +10,28 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(ROUTES);
-app.use(cors());
+
+app.use(function(req, res, next) {
+  // Website you wish to allow to connect
+  res.header("Access-Control-Allow-Origin", "http://localhost:8888");
+
+  // Request methods you wish to allow
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.header("Access-Control-Allow-Headers", "X-Requested-With,content-type");
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.header("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () =>
   console.log("connected to DB!")
 );
