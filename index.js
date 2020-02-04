b3172f7954a82c4452a57013f2a6a96d66e82fcf
animewoman <bekameilun@gmail.com>
2020-02-04 20:58:44 +0600

    added cors

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-
+const cors = require("cors");
 const PORT = process.env.PORT || 5000;
 
 const ROUTES = require("./route");
@@ -10,30 +10,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(ROUTES);
-app.use(function(req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "*");
-
-  // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-
-  // Request headers you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true);
-
-  // Pass to next layer of middleware
-  next();
-});
-
+app.use(cors());
 mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () =>
   console.log("connected to DB!")
 );
